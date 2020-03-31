7f7538cc75483d30c5ce4453fcca95e1a580ad1a
roni <ronigoldsmid@gmail.com>
2020-03-31 17:35:03 +0300

    commit operation

---
+++ test.py
@@ -0,0 +1,34 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.4.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+x = 10
+
+y = 8
+
+x+y
+
+# +
+x = 1
+
+y = 3
+# -
+
+# hi 
+
+x + y
+
+x = 10 
+y = 90
+x*y
